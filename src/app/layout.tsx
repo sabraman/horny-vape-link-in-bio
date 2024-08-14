8f2a9b1524149ee3f978af85d72326febb7b58d7
sabraman <danyayudin2012@gmail.com>
2024-08-14 14:58:44 +0300

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,40 +8,40 @@ import Script from "next/script";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "HORNY VAPE | ВЕЙПШОП",
-  description: "HORNY VAPE",
-  metadataBase: new URL("https://hrny.ru/"),
-  alternates: {
-    canonical: "/",
-  },
-  openGraph: {
-    type: "website",
-    url: "https://hrny.ru/",
-    title: "HORNY VAPE | ВЕЙПШОП",
-    description: "HORNY VAPE",
-    siteName: "HORNY VAPE",
-    images: "./og-image.png",
-  },
-  icons: {
-    icon: "./favicons/favicon.svg",
-    apple: "./favicons/apple-touch-icon.png",
-  },
+	title: "HORNY PLACE",
+	description: "HORNY PLACE",
+	metadataBase: new URL("https://horny.place/"),
+	alternates: {
+		canonical: "/",
+	},
+	openGraph: {
+		type: "website",
+		url: "https://horny.place/",
+		title: "HORNY PLACE",
+		description: "HORNY PLACE",
+		siteName: "HORNY PLACE",
+		images: "./og-image.png",
+	},
+	icons: {
+		icon: "./favicons/favicon.svg",
+		apple: "./favicons/apple-touch-icon.png",
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ru">
-      <head>
-        <meta name="yandex-verification" content="eaf2e22c2dc6f579" />
-      </head>
-      <body className={montserrat.className}>
-        {children}
-        <Script src="https://ticketscloud.com/static/scripts/widget/tcwidget.js" />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ru">
+			<head>
+				<meta name="yandex-verification" content="eaf2e22c2dc6f579" />
+			</head>
+			<body className={montserrat.className}>
+				{children}
+				<Script src="https://ticketscloud.com/static/scripts/widget/tcwidget.js" />
+			</body>
+		</html>
+	);
 }
